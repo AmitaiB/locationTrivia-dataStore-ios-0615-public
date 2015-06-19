3c6d4ca9ebfa4639c3e83a48000b0e8355e7db0a
AmitaiB <blickstein@gmail.com>
2015-06-19 18:14:26 -0400

    adds viewWillAppear to reload data (unsure why needed)

--- FISAddLocationViewController.m
+++ FISAddLocationViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "FISAddLocationViewController.h"
+#import "FISLocation.h"
+#import "FISLocationsTableViewController.h"
+#import "FISLocationsDataManager.h"
 
 @interface FISAddLocationViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *latitudeTextField;
+@property (weak, nonatomic) IBOutlet UITextField *longitudeTextField;
+- (IBAction)cancelButtonTapped:(id)sender;
+- (IBAction)doneButtonTapped:(id)sender;
 
 @end
 
@@ -24,14 +32,50 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+
+    
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
-*/
 
+-(BOOL)inputIsValid
+{
+    BOOL hasName = self.nameTextField.text.length > 0;
+    BOOL hasLatitude = self.latitudeTextField.text.length > 0;
+    BOOL hasLongitude = self.longitudeTextField.text.length > 0;
+    
+    return hasName && hasLatitude && hasLongitude;
+}
+
+- (IBAction)cancelButtonTapped:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)doneButtonTapped:(id)sender {
+    if (![self inputIsValid]) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Invalid input" message:@"All fields required, dude." preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+        [alert addAction:okAction];
+        
+        [self presentViewController:alert animated:YES completion:nil];
+        
+        return;
+    }
+    
+    
+    FISLocation *newLocation = [[FISLocation alloc] initWithName:self.nameTextField.text latitude:@([self.latitudeTextField.text integerValue]) longitude:@([self.longitudeTextField.text integerValue])];
+    
+    FISLocationsDataManager *dataManager = [FISLocationsDataManager sharedLocationsDataManager];
+    
+    [dataManager.locations addObject:newLocation];
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+
+}
 @end
--- locationTrivia-dataStore/FISLocationsTableViewController.m
+++ locationTrivia-dataStore/FISLocationsTableViewController.m
@@ -73,6 +73,12 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [self.tableView reloadData];
+}
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
